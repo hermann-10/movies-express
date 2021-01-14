2ce8796807ee3e2693c4cbb6013d711eb31ef1c1
Hermann Manuel <hermann.manuel@hotmail.com>
2021-01-14 18:08:02 +0100

    Création d'un formulaire et gestion du POST

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 const PORT = 3000;
 
 app.use('/public', express.static('public')); //.use permet d'indiquer quel middleware on aimerait ajouter
+//app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
@@ -19,9 +21,12 @@ app.get('/movies', (req, res) => {
     res.render('movies', { movies: frenchMovies , title: title }) //pour utiliser un template il faut utiliser la méthode render, il sait automatiquement qu'on utilise un template. Pas besoin également de préciser le format .ejs car on déclarer ceci plus haut : app.set('view engine', 'ejs');
 });
 
-//app.get('/movie-details', (req, res) => {
-  //  res.render('movie-details');
-//});
+let urlEncodedParser = bodyParser.urlencoded({ extended: false });
+
+app.post('/movies', urlEncodedParser, (req, res) => {
+    console.log(req.body);
+    res.sendStatus(201);
+}); 
 
 app.get('/movies/add', (req, res) => {
     res.send('Prochainement, un formulaire pour réaliser des ajouts ici)');
